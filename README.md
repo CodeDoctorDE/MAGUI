4fcb0a8f57b912e885e0d1337c9203d4886c84fe
CodeDoctorDE <wusel905@gmail.com>
2020-10-05 16:16:43 +0200

    rename to magui, remove idea

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MapMods
+# MUI
+
 Create interfaces with maps in minecraft!
+
+The all in one **MA**P **G**RAPHICAL **U**SER **I**NTERFACE system!
--- app/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.github.codedoctorde.mapmods.app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.github.codedoctorde.mapmods.app to javafx.fxml;
-    exports com.github.codedoctorde.mapmods.app;
-}
